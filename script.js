e6224ce94007dc7b65f407a89d3c03cf66dd66c8
elsdenl14 <elsdenliam4@gmail.com>
2026-06-23 13:12:17 +0200

    Update script.js

--- script.js
+++ script.js
@@ -481,7 +481,6 @@ function getFiltered() {
   if (ownedFilter==='owned') list = list.filter(c => c.owned!==false);
   if (ownedFilter==='missing') list = list.filter(c => c.owned===false);
   if (ownedFilter==='wishlist') list = list.filter(c => c.wished===true);
-  if (ownedFilter==='legend') list = list.filter(c => c.rarity === 'legend');
   list.sort((a,b) => {
     if (sort==='name') return (a.make+a.name).localeCompare(b.make+b.name);
     if (sort==='make') return (a.make||'').localeCompare(b.make||'');
